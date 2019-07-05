b3c70d9e80721f5b112a4c93e0abcb157f9b6827
Jachym Cepicky <jachym.cepicky@gmail.com>
2019-07-05 12:55:05 +0200

    adding setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+
+setup(name='kg_reservation',
+      author="Jachym Cepicky",
+      author_email="jachym.cepicky@gmail.com",
+      url="https://github.com/jachym/kindergarten-reservation",
+      version='1.0',
+      packages=find_packages())
