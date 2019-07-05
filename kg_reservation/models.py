69a76680401452104ec0e4b96a3f7d75a361289e
Jachym Cepicky <jachym.cepicky@gmail.com>
2019-07-05 12:50:40 +0200

    init

---
+++ kg_reservation/models.py
@@ -0,0 +1,60 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.utils.translation import gettext as _
+
+# Create your models here.
+
+class Kindergarten(models.Model):
+
+    name = models.CharField(
+        max_length=50,
+        help_text=_("The name of the Kindergarten")
+    )
+
+    address = models.TextField(
+        help_text=_("Precise address")
+    )
+
+   phone = models.CharField(
+       max_length=20,
+    )
+
+    email = models.EmailField()
+
+    web = models.URLField()
+
+    note = models.TextField(blank=True)
+
+class Teacher(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    kindergarten = models.ForeignKey("Kindergarten")
+
+class Parent(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    kindergarten = models.ForeignKey("Kindergarten")
+
+class Child(models.Model):
+
+    first_name = models.CharField(
+        max_length=20)
+
+    last_name = models.CharField(
+        max_length=20)
+
+    middle_name = models.CharField(
+        max_length=50,
+        blank=True
+    )
+
+    kindegarten = models.ForeignKey("Kinderkarten")
+    parent = models.ForeignKey("Parent")
+
+class Day(models.Model):
+
+    capacity = models.IntegerField()
+    date = models.DateField()
+    note = models.TextField(blank=True)
+    
+    child = models.ManyToManyField("Child")
+    teacher = models.ManyToManyField("Teacher")
+
